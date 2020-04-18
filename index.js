7cf385cb44d1b6526d5a932fc296cb3fad575eea
Thiibo <Thiibo@users.noreply.github.com>
2020-04-18 17:21:02 +0200

    Added HEX into color picker, started work on UserScript generation

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ Element.prototype.index = function() {
   return i;
 }
 
+function open(text) {
+  let a = document.createElement('a');
+  a.href = 'data:text/javascript;charset=utf-8,' + encodeURIComponent(text);
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  delete a;
+}
+
+const $install = document.getElementById('install');
+
 let theme = 1;
 const $customthemecol = document.getElementById('customthemecolor');
 
@@ -25,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   let $colorpickers = document.querySelectorAll('input[type="color"]');
   let colorpickers = [];
-  for (var i = 0; i < $colorpickers.length; i++) colorpickers.push(new CP($colorpickers[i]));
+  for (var i = 0; i < $colorpickers.length; i++) colorpickers.push(new CP($colorpickers[i], {color: 'HEX'}));
 
   for (var i = 0; i < $colorpickers.length; i++) {
     $colorpickers[i].addEventListener('click', function(e) {
@@ -33,10 +44,50 @@ document.addEventListener("DOMContentLoaded", function() {
     }, false);
     colorpickers[i].on('change', function(r, g, b) {
       this.source.value = this.color(r, g, b, 1);
+      let $cHex = document.getElementsByClassName('color-picker:hex');
+      if ($cHex.length > 0) $cHex[0].firstChild.value = this.source.value;
     });
   }
 
   $customthemecol.addEventListener('click', function() {
     this.checked ? this.nextElementSibling.classList.add('open') : this.nextElementSibling.classList.remove('open');
-  })
+  });
+  $customthemecol.checked ? $customthemecol.nextElementSibling.classList.add('open') : $customthemecol.nextElementSibling.classList.remove('open');
+
+
+  $install.addEventListener('click', function() {
+    let final = `
+    // ==UserScript==
+    // @name          Dark Scratch Editor
+    // @namespace     https://thiibo.github.io
+    // @description   Want to code with Scratch, but the editor is too bright? This style applies a dark theme to the online Scratch (3) editor so you can code in peace.
+    // @author        Thiibo
+    // @homepage      https://thiibo.github.io/dark-scratch-editor/
+    // @run-at        document-start
+    // @version
+    // ==/UserScript==
+    (function() {var css = "";
+    if (false || (new RegExp("^((https?:\\/\\/)?scratch\\.mit\\.edu\\/projects\\/(\\d*\\/)?(editor|fullscreen).*)$")).test(document.location.href))
+    	css += [
+    `
+    let themelink;
+    switch (theme) {
+      case 0:
+        themelink = "./themes/classic.css"
+        break;
+      case 1:
+        themelink = "./themes/seablue.css"
+        break;
+      case 2:
+        themelink = "./themes/deepblack.css"
+        break;
+      default:
+        themelink = "./themes/classic.css"
+    }
+    fetch(themelink)
+    .then( response => response.text() )
+    .then( text => console.log(text) )
+    console.log(final);
+    //open(final);
+  });
 });
---
+++ script.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name          Hello jQuery
+// @namespace     http://www.example.com/
+// @description   jQuery test script
+// @include       *
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// ==/UserScript==
