31f20dd6cd9ab32e7285f2630538935f76c632a7
Thiibo <Thiibo@users.noreply.github.com>
2020-04-18 17:51:02 +0200

    Userscripts should now be able to generate properly.

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ const $install = document.getElementById('install');
 
 let theme = 1;
 const $customthemecol = document.getElementById('customthemecolor');
+const $customethemecolors = [
+    document.getElementById('primarycolor'),
+    document.getElementById('secundarycolor')
+]
 
 document.addEventListener("DOMContentLoaded", function() {
   const $selecthead = document.getElementsByClassName('select-head');
@@ -70,6 +74,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (false || (new RegExp("^((https?:\\/\\/)?scratch\\.mit\\.edu\\/projects\\/(\\d*\\/)?(editor|fullscreen).*)$")).test(document.location.href))
     	css += [
     `
+    // FETCH CSS
     let themelink;
     switch (theme) {
       case 0:
@@ -84,10 +89,58 @@ document.addEventListener("DOMContentLoaded", function() {
       default:
         themelink = "./themes/classic.css"
     }
+    let themecss;
     fetch(themelink)
     .then( response => response.text() )
-    .then( text => console.log(text) )
+    .then( text => themecss = text.split(/\r?\n/); );
+    let style;
+    fetch("./userstyle.css")
+    .then( response => response.text() )
+    .then( text => style = text.split(/\r?\n/); );
+    let customcolorscss;
+    if ($customthemecol.checked) {
+      fetch("./customcolors.css")
+      .then( response => response.text() )
+      .then( text => customcolorscss = text.split(/\r?\n/); );
+    } else {
+      customcolorscss = "// Custom theme colors disabled";
+    }
+
+    let j = 0;
+    for (var i = 0; i < themecss.length; i++) {
+      if (themecss[i].trim() == '%s') {
+        final += '"' + $customethemecolors[j].value + '",';
+        j++;
+      } else {
+        final += '"' + themecss[i] + '",';
+      }
+    }
+
+
+    for (var i = 0; i < style.length; i++) final += '"' + (style[i].trim() == '%s' ? customcolorscss : style[i]) + '"' + (i < style.length - 1 ? ',' : '');
+
+    final += `].join("\n");
+if (typeof GM_addStyle != "undefined") {
+  GM_addStyle(css);
+} else if (typeof PRO_addStyle != "undefined") {
+  PRO_addStyle(css);
+} else if (typeof addStyle != "undefined") {
+  addStyle(css);
+} else {
+  var node = document.createElement("style");
+  node.type = "text/css";
+  node.appendChild(document.createTextNode(css));
+  var heads = document.getElementsByTagName("head");
+if (heads.length > 0) {
+  heads[0].appendChild(node);
+} else {
+  // no head yet, stick it whereever
+  document.documentElement.appendChild(node);
+}
+}
+})();`
+
     console.log(final);
-    //open(final);
+    open(final);
   });
 });
