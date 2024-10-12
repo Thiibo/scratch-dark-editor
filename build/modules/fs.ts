cb120d89e5ba769c3adb2596672297dcaa56cecc
Thiibo <40576021+Thiibo@users.noreply.github.com>
2024-10-13 01:12:50 +0200

    Add script to read the theme files in the `style` directory

---
+++ build/modules/fs.ts
@@ -0,0 +1,29 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+
+const THEMES_PATH = "../../style/themes";
+
+type Theme = {
+    path: string,
+    name: string,
+    content: string
+}
+
+async function getThemes(): Promise<Theme[]> {
+    const files = await fs.readdir(THEMES_PATH);
+    return Promise.all(files.map(getThemeFileInfo));
+}
+
+async function getThemeFileInfo(filePath: string): Promise<Theme> {
+    const absolutePath = path.resolve(filePath);
+    const name = path.basename(absolutePath).split('.')[0];
+    const content = await fs.readFile(absolutePath, 'utf-8');
+    
+    return {
+        path: absolutePath,
+        name: name,
+        content: content
+    }
+}
+
+export { getThemes };
