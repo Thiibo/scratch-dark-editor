0c9f781244d362a321105a4584ba26afd01d5a31
Thiibo <40576021+Thiibo@users.noreply.github.com>
2024-10-13 01:48:43 +0200

    Reorganise generate-class-mappings script

--- build/generate-class-mappings.ts
+++ build/generate-class-mappings.ts
@@ -1,15 +1,15 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 import { extractAllRegexCaptureGroups, interpretKeyValueObjectString } from "./modules/helper-functions";
 
-const MAPPING_EXTRACTION_REGEX = /a\.locals\s*=\s*\{([^}]*)\}/g;
-const SCRATCH_WEBPACK_BUNDLE_SOURCE = "https://scratch.mit.edu/js/projects.bundle.js"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-type StringKeyValueObject = {[name: string]: string}
+const MAPPING_EXTRACTION_REGEX = /a\.locals\s*=\s*\{([^}]*)\}/g;
+const SCRATCH_WEBPACK_BUNDLE_SOURCE = "https://scratch.mit.edu/js/projects.bundle.js"
 
+type StringKeyValueObject = {[name: string]: string}
 async function getClassMappings(): Promise<StringKeyValueObject> {
     const res = await fetch(SCRATCH_WEBPACK_BUNDLE_SOURCE);
     const webpackCode = await res.text();
@@ -20,4 +20,5 @@ async function getClassMappings(): Promise<StringKeyValueObject> {
 
 const outPath = path.join(__dirname, 'class-mappings.json');
 const classMappings = await getClassMappings();
+
 fs.writeFileSync(outPath, JSON.stringify(classMappings));
